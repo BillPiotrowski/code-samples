f879bbc7ff1a02d28338cb7abcb9ebce4f82f6c8
Bill Piotrowski <billpiotrowski@me.com>
2026-06-08 23:49:14 -0700

    general cleanup.

--- react-ios-nav/src/App.tsx
+++ react-ios-nav/src/App.tsx
@@ -1,28 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import getAppRouter from './AppRouter';
-import useIsSingleColumnLayout from './Utility/isSingleColumnEffect';
 import SplashScreen from './Component/SplashScreen';
 import { API } from './API';
-// import { GetAssetFiltersResponse } from '../../data/Asset/Asset';
-// import { Ruleset } from '../../data/Ruleset/Ruleset';
-
-
 
 interface AppMockArgs {
     baseUrl: string;
-    // accessTokenString: string;
-    // endpointUrl: string;
-    // userId: string;
 }
 
 const App: React.FC<AppMockArgs> = args => {
-//  const mirrorscapeAPI = new MirrorscapeAPI(args.accessTokenString, args.endpointUrl);
-    // const mirrorscapeUser: MirrorscapeUser = {username: "test"}
-    const useSingleColumn = useIsSingleColumnLayout();
-    // const [assetFilters, setAssetFilters] = useState<GetAssetFiltersResponse|null>(null);
-    // const [rulesets, setRulesets] = useState<Ruleset[]|null>(null);
-
     const [hasLoaded, setHasLoaded] = useState(false);
 
     /**
@@ -33,18 +19,6 @@ const App: React.FC<AppMockArgs> = args => {
             console.log("completed loading.");
             setHasLoaded(true);
         });
-        // const temp = setTimeout(resolve, 2000)).then(() => {
-        //     // your code here
-        // }
-        // mirrorscapeAPI.getAdminAPI().getAllAssetFilters().then(f => {
-        //     setAssetFilters(f);
-        //     return mirrorscapeAPI.getCharacterAPI().listRulesets(10, 1, 'name', 'asc', {});
-        // }).then(r => {
-        //     setRulesets(r.items);
-        // }).catch((e)=>{
-        //     console.log(`Error while fetching asset filters.`);
-        //     console.log(e);
-        // });
 
     }, []);
 
@@ -55,11 +29,6 @@ const App: React.FC<AppMockArgs> = args => {
     const router = getAppRouter(
         args.baseUrl,
         new API(),
-        // mirrorscapeUser,
-        // args.userId,
-        useSingleColumn,
-        // assetFilters,
-        // rulesets
     );
 
     return (
